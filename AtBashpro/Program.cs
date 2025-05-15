d915848daf8a78d9d023239ff0edd20d03989153
Daniel Fromer <daniel.fromer1@gmail.com>
2025-05-15 12:10:42 +0300

    uptate

--- AtBashpro/Program.cs
+++ AtBashpro/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AtBashpro
+namespace atbash
 {
     internal class Program
     {
+        static string AtbashDecrypt(string encryptedmassage)
+        {
+            encryptedmassage = encryptedmassage.Replace("\n", " \n ");
+            string decrypted = "";
+            foreach (char c in encryptedmassage)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (char.IsUpper(c))
+                    {
+                        decrypted += (char)('A' + ('Z' - c));
+                    }
+                    else
+                    {
+                        decrypted += (char)('a' + ('z' - c));
+                    }
+
+                }
+                else
+                {
+                    decrypted += c;
+                }
+            }
+            return decrypted;
+        }
         static void Main(string[] args)
         {
+
+
         }
+
     }
 }
+
